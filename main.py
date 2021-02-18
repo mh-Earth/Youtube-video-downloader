810e90c9c7029a3f857f2c1aa0f704f68f254440
mh-Earth <meherab0608@gmail.com>
2021-02-18 15:44:58 +0600

    fix pydub bugs

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from banners import randomBanners
 
 if __name__ == "__main__":
     
-    if sys.platform == "darwin" or "xdg-open":
-        if not os.geteuid() == 0:
-            sys.exit("""\033[1;91m\n[!] This srcipt must be run as root or administrator. ¯\_(ツ)_/¯\n\033[1;m""")
-    else:
-        pass
+#    if sys.platform == "darwin" or "xdg-open":
+#        if not os.geteuid() == 0:
+#            sys.exit("""\033[1;91m\n[!] This srcipt must be run as root or #administrator. ¯\_(ツ)_/¯\n\033[1;m""")
+#    else:
+#        pass
 
 
     colors = [Fore.RED, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
@@ -112,9 +112,14 @@ if __name__ == "__main__":
     sleep(3)
     # ____________________________________
     if videoType == "mp3" or videoType == "audio":
-        flac_audio = AudioSegment.from_file(f"{PATH}/{fileName}")
-        flac_audio.export(f"{PATH}/{fileName[:-5]}" + ".mp3", format="mp3")
-        os.system(f"del /f {PATH}/{fileName}") if sys.platform == "win32" else os.system(f"rm -r {PATH}/{fileName}")
+        try:
+            flac_audio = AudioSegment.from_file(f"{PATH}/{fileName}")
+            flac_audio.export(f"{PATH}/{fileName[:-5]}" + ".mp3", format="mp3")
+            os.system(f"del /f {PATH}/{fileName}") if sys.platform == "win32" else os.system(f"rm -r {PATH}/{fileName}")
+        except Exception as e:
+            print(e)
+            if sys.platform == "darwin" or "xdg-open":
+                print("try 'sudo apt install ffmpeg'")
 
     # ____________________________________
     print("\033[92m[+]Download completed\033[92m")
